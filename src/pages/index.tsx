ea7428a19cdcc3234f064880bc04044d1f296412
tslnigeriatv <57802488+tslnigeriatv@users.noreply.github.com>
2023-01-30 06:00:05 +0100

    used codemod

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,8 @@ export default function Home({
   romanceMovies,
   documentaries
 }: Props) {
+
+  
   
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
@@ -86,6 +88,8 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
   ])
+
+  // console.log(netflixOriginals)
 
   return {
     props: {
